a8c010fd117d0883e64524117acf78adc9f579c5
qinghe <wangqinghe@youzan.com>
2020-05-12 13:42:14 +0800

    feat: 平滑颜色

--- 02 - 三角形/opengl/main.c
+++ 02 - 三角形/opengl/main.c
@@ -3,10 +3,15 @@ void display()
  {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(-0.5, 0.5);
-        glVertex2f(0.5, 0.5);
-        glVertex2f(0.5, -0.5);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(-0.5, -0.5);
+    
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(-0.5, 0.5);
+    
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2f(0.5, 0.5);
+ 
     glEnd();
     glFlush();
 }
